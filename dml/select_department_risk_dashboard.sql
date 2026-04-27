850b36a2f2c969f233f5ac321e2033f8cb06b865
ask4anshuman <159439067+ask4anshuman@users.noreply.github.com>
2026-04-27 09:50:59 +0100

    chore(sql-doc): add confluence link to dml/select_department_risk_dashboard.sql

--- dml/select_department_risk_dashboard.sql
+++ dml/select_department_risk_dashboard.sql
@@ -1,7 +1,7 @@
 -- Department Performance + Risk Dashboard (MySQL 8+)
 -- One large SELECT using multiple CTEs, joins, translations, and filters.
 -- Tables: employees, departments, employee_enriched_target, etl_run_audit, etl_employee_rejects
--- Confluence: https://ask4anshuman.atlassian.net/wiki/pages/viewpage.action?pageId=5898260
+-- Confluence: https://ask4anshuman.atlassian.net/wiki/pages/viewpage.action?pageId=6422529
 
 WITH
 latest_success_run AS (
